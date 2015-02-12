e650369cf2e6bb1b8937fa5f5563ae73feb7de1c
XiaoYulong <xiaoyulong007@gmail.com>
2015-02-12 13:35:08 +0800

    add node view

---
+++ YLSwipeLockView/YLSwipeLockNodeView.h
@@ -0,0 +1,13 @@
+//
+//  YLSwipeLockNodeView.h
+//  YLSwipeLockViewDemo
+//
+//  Created by 肖 玉龙 on 15/2/12.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLSwipeLockNodeView : UIView
+
+@end
---
+++ YLSwipeLockView/YLSwipeLockNodeView.m
@@ -0,0 +1,21 @@
+//
+//  YLSwipeLockNodeView.m
+//  YLSwipeLockViewDemo
+//
+//  Created by 肖 玉龙 on 15/2/12.
+//  Copyright (c) 2015年 Yulong Xiao. All rights reserved.
+//
+
+#import "YLSwipeLockNodeView.h"
+
+@implementation YLSwipeLockNodeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
